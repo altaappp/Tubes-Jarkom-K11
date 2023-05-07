b4f6da1261f05bdbdde1cf34fb89d395d01c2977
Altaff <121285212+altaappp@users.noreply.github.com>
2023-05-07 16:55:47 +0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,29 +1,42 @@
- from socket import *
+#Mengimport socket
+from socket import *
 
 serverName = 'localhost'
+# Menetapkan nomor port server
 serverPort = 12000
+
+# Membuat socket server
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
+
+# Menerima permintaan koneksi dari client
 serverSocket.listen(1)
 print('Server is Ready...')
 
 while True:
-    # Establish the connection
+    # Membuat koneksi antara client dan server
     connectionSocket, addr = serverSocket.accept()
     try:
+        # Memasukkan data paket ke variabel message
         message = connectionSocket.recv(1024)
         filename = message.split()[1]
+
+        # Membuka dan membaca file yang diminta client
         f = open(filename[1:], 'rb')
         outputdata = f.read()
+
+        # Menutup file
         f.close()
-        # Send the HTTP status code and content of the requested file to the client
+
+        # Mengirim status code dan konten file yang diminta oleh client
         connectionSocket.send(b'HTTP/1.0 200 OK\r\n\r\n' + outputdata)
         connectionSocket.close()
+
     except IOError:
-        # Send response message for file not found
+        # Mengirim respons berupa status code apabila file tidak ditemukan
         response_header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html\n\n'
-        response_data = b'<html><body><h1>404 Not Found</h1><</body></html>'
-        
+        response_data = b'<html><body><h1>404 Not Found</h1><h3>The file you\'re looking for does not exist.</h3></body></html>'
+
         response = response_header.encode() + response_data
         connectionSocket.sendall(response)
 
